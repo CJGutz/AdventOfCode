119a78b413e476883b7b1f0139bbaff84e4f27a0
CJGutz <cjgutzkow@gmail.com>
2021-12-08 13:42:15 +0100

    december 7th

---
+++ Dec7Part2.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Dec7Part2 {
+    public static void main(String[] args) {
+        FileReader fr;
+        StringBuilder sb = new StringBuilder();
+        try {
+            fr = new FileReader("C:\\Users\\cjgut\\Desktop\\Advent of code\\inputdec7.txt");
+            int i = fr.read();
+            while (i != -1) {
+                sb.append((char) i);
+                i = fr.read();
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        String text = sb.toString();
+
+        String[] inputs = text.split(",");
+        
+        ArrayList<Integer> positions = new ArrayList<>();
+        for (int i = 0; i < inputs.length; i++) {
+            positions.add(Integer.parseInt(inputs[i].trim()));
+        }
+        
+        int sum = 0;
+        for (int position : positions)
+            sum += position;
+        int average = (int) (sum / positions.size());
+
+        int fuelA = 0;
+        for (int position : positions) {
+            int difference = Math.abs(position - average);
+            fuelA += (difference * (difference + 1))/2;
+        }
+
+        System.out.println(fuelA);
+    }
+}
