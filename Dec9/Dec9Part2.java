e07700690a42c2eace5ecf1d78dee4c7a3be7e3e
Carl Gützkow <cjgutzkow@gmail.com>
2022-11-30 20:08:17 +0100

    add december 9th 2021 to main branch

---
+++ Dec9/Dec9Part2.java
@@ -0,0 +1,95 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Dec9Part2 {
+    public static void main(String[] args) {
+        FileReader fr;
+        StringBuilder sb = new StringBuilder();
+        try {
+            fr = new FileReader("C:\\Users\\cjgut\\Desktop\\Advent of code\\inputdec9.txt");
+            int i = fr.read();
+            while (i != -1) {
+                sb.append((char) i);
+                i = fr.read();
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        String text = sb.toString();
+
+        String[] inputs = text.split("\n");
+
+        for (int i = 0; i < inputs.length; i++) {
+            inputs[i] = inputs[i].trim(); // trim all String so no whitespace is caught
+        }
+
+
+        ArrayList<int[]> stack = new ArrayList<>(); // A queue of which cells is to be visited first
+        ArrayList<int[]> visited = new ArrayList<>(); // List of cells that are visited
+        ArrayList<Integer> basins = new ArrayList<>(); // List of number of cells in each basin
+
+        StringBuilder graphical = new StringBuilder(); // String for graphical basins
+
+        for (int i = 0; i < inputs.length; i++) { // Loops through all lines
+            for (int j = 0; j < inputs[i].length(); j++) { // Loops through all characters
+
+                int sumBasin = 0; // Number of cells in one basin
+
+                String color = "\033[0m"; // Reset color
+
+                if (inputs[i].charAt(j) != '9') { // Dont visit any nines
+                    color = "\033[48;2;0;" + (10 + 20 * Integer.parseInt(String.valueOf(inputs[i].charAt(j)))) + ";50m"; // change to color dynamic to number
+                    int[] n = {i,j}; 
+                    stack.add(n);
+                }
+
+                graphical.append(color + "  "); // Print number with color
+
+                while (!stack.isEmpty()) {
+                    sumBasin += 1; // Adds up as long as there are availble cells that are not nines, not visited or already in the stack
+
+                    int[] next = stack.remove(0); // Visit the first cell in the stack
+                    visited.add(next); // say it is visited
+
+
+                    for (int y = -1; y <= 1; y++)
+                        for (int x = -1; x <= 1; x++)
+                            if (x * y == 0 && x != y) { // Use only certain x's and y's
+                                int yCell = next[0] + y;
+                                int xCell = next[1] + x;
+                                if (yCell >= 0 && yCell <= inputs.length - 1 && xCell >= 0 && xCell <= inputs[next[0]].length() - 1) // Check if cell is out of bounds
+                                    if (inputs[yCell].charAt(xCell) != '9' && !inList(yCell, xCell, visited) && !inList(yCell, xCell, stack)) { // Check if cell is not a nine, not visited or already in the stack
+                                        int[] newNext = {yCell, xCell}; 
+                                        stack.add(0, newNext); // Add available cell to stack
+                                    }
+                            }
+                }  
+                
+                if (sumBasin > 0) { // sumBasin == 0 means the cell was a 9
+                    basins.add(sumBasin); // Add number of cells in one basin to a list
+                }
+            }
+            graphical.append("\033[0m\n");
+        }
+        
+        Collections.sort(basins, Collections.reverseOrder());
+        System.out.println(basins.get(0) * basins.get(1) * basins.get(2)); // Solution given there are more than three basins
+        System.out.println(graphical.toString()); // Print out graphical basins
+    }
+
+    public static boolean inList(int nextY, int nextX, ArrayList<int[]> list) {
+        for (int[] c : list) {
+            if (nextY == c[0] && nextX == c[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
---
+++ Dec9/Dec9Part2Animation.java
@@ -0,0 +1,160 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import javax.swing.*;
+import java.awt.event.*;
+
+class Dec9Part2Animation {
+
+    private static JFrame frame = new JFrame();
+    private static JPanel panel = new JPanel();
+    private static JLabel txt = new JLabel();
+    private static String[] inputs;
+    private static ArrayList<int[]> visited;
+
+    public static void main(String[] args) {
+
+        makeFrame();
+
+        FileReader fr;
+        StringBuilder sb = new StringBuilder();
+        try {
+            fr = new FileReader("C:\\Users\\cjgut\\Desktop\\Advent of code\\inputdec9.txt");
+            int i = fr.read();
+            while (i != -1) {
+                sb.append((char) i);
+                i = fr.read();
+            }
+            fr.close();
+        } catch (FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        String text = sb.toString();
+
+        inputs = text.split("\n");
+
+        for (int i = 0; i < inputs.length; i++) {
+            inputs[i] = inputs[i].trim(); // trim all String so no whitespace is caught
+        }
+
+
+        ArrayList<int[]> stack = new ArrayList<>(); // A queue of which cells is to be visited first
+        visited = new ArrayList<>(); // List of cells that are visited
+        ArrayList<Integer> basins = new ArrayList<>(); // List of number of cells in each basin
+
+        StringBuilder graphical = new StringBuilder(); // String for graphical basins
+
+        for (int i = 0; i < inputs.length; i++) { // Loops through all lines
+            for (int j = 0; j < inputs[i].length(); j++) { // Loops through all characters
+
+                int sumBasin = 0; // Number of cells in one basin
+
+                String color = "\033[0m"; // Reset color
+                
+                int[] n = {i,j}; 
+                if (inputs[i].charAt(j) != '9') { // Dont visit any nines
+                    color = "\033[48;2;0;" + (10 + 20 * Integer.parseInt(String.valueOf(inputs[i].charAt(j)))) + ";50m"; // change to color dynamic to number
+                    stack.add(n);
+                }
+
+                graphical.append(color + "  "); // Print with color
+
+                while (!stack.isEmpty()) {
+                    sumBasin += 1; // Adds up as long as there are availble cells that are not nines, not visited or already in the stack
+
+                    int[] next = stack.remove(0); // Visit the first cell in the stack
+                    if (!inList(next[0], next[1], visited))
+                        visited.add(next); // say it is visited
+
+
+                    for (int y = -1; y <= 1; y++)
+                        for (int x = -1; x <= 1; x++)
+                            if (x * y == 0 && x != y) { // Use only certain x's and y's
+                                int yCell = next[0] + y;
+                                int xCell = next[1] + x;
+                                if (yCell >= 0 && yCell <= inputs.length - 1 && xCell >= 0 && xCell <= inputs[next[0]].length() - 1) // Check if cell is out of bounds
+                                    if (inputs[yCell].charAt(xCell) != '9' && !inList(yCell, xCell, visited) && !inList(yCell, xCell, stack)) { // Check if cell is not a nine, not visited or already in the stack
+                                        int[] newNext = {yCell, xCell}; 
+                                        stack.add(0, newNext); // Add available cell to stack
+                                    }
+                            }
+                }  
+                
+                if (sumBasin > 0) { // sumBasin == 0 means the cell was a 9
+                    basins.add(sumBasin); // Add number of cells in one basin to a list
+                }
+            }
+            graphical.append("\033[0m\n");
+        }
+        
+        Collections.sort(basins, Collections.reverseOrder());
+        System.out.println(basins.get(0) * basins.get(1) * basins.get(2)); // Solution given there are more than three basins
+        System.out.println(graphical.toString()); // Print out graphical basins
+
+        animateSwing(visited, inputs);
+        //animateTerminal(visited, inputs);
+    }
+
+    public static boolean inList(int nextY, int nextX, ArrayList<int[]> list) {
+        for (int[] c : list) {
+            if (nextY == c[0] && nextX == c[1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void makeFrame() {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        txt.setHorizontalAlignment(0);
+        panel.add(txt);
+        frame.add(panel);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+    }
+
+    public static void animateSwing(ArrayList<int[]> visited, String[] inputs) {
+        String[][] cells = new String[inputs.length][inputs[0].length()];
+        for (int[] vC : visited) {
+            cells[vC[0]][vC[1]] = "<b style=\"background-color:rgb(0, " + (10 + 20 * Integer.parseInt(String.valueOf(inputs[vC[0]].charAt(vC[1])))) + ", 50)\">" + "_ " + " </b>";
+
+            StringBuilder sb = new StringBuilder();
+            sb.append("<html>");
+            for (int i = 0; i < cells.length; i++) {
+                for (int j = 0; j < cells[i].length; j++) {
+                    if (cells[i][j] == null) {
+                        sb.append("_ ");
+                    } else sb.append(cells[i][j]);
+                }
+                sb.append("<br/>");
+            }
+            sb.append("</html>");
+
+            txt.setText(sb.toString());
+        }
+
+    }
+
+
+    public static void animateTerminal(ArrayList<int[]> visited, String[] inputs) {
+        String[][] cells = new String[inputs.length][inputs[0].length()];
+        for (int[] vC : visited) {
+            cells[vC[0]][vC[1]] = "\033[48;2;0;" + (10 + 20 * Integer.parseInt(String.valueOf(inputs[vC[0]].charAt(vC[1])))) + ";50m" + "  " + "\033[0m" ;
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < cells.length; i++) {
+                for (int j = 0; j < cells[i].length; j++) {
+                    if (cells[i][j] == null) {
+                        sb.append("  ");
+                    } else sb.append(cells[i][j]);
+                }
+                sb.append("\n");
+            }
+            System.out.print("\033[H\033[2J" + sb.toString());
+        }
+    }
+}
